6df2a1afab4b6de4f3fb78072482d906526aae2e
Bernardo Pedriali Redmerski <bernardopred@gmail.com>
2026-07-28 10:12:52 -0300

    Atualizar o Ex6.c

--- Ex6.c
+++ Ex6.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
- 
 int main() {
- 
  double x, y, z;
- 
  scanf("%lf %lf", &y, &z);
- 
  double yy = y * 3.5;
  double zz = z * 7.5;
- 
  double zy = yy + zz;
  x = zy / 11;
- 
  printf("MEDIA = %.5lf\n", x);
- 
     return 0;
 }
