241ca1c1a5d52ea38323872b829abdc06f6b722b
Bernardo Pedriali Redmerski <bernardopred@gmail.com>
2026-08-11 08:19:13 -0300

    Update Cédulas.c

--- Cédulas.c
+++ Cédulas.c
@@ -9,6 +9,6 @@ int main() {
     e = ((num-(100*a+50*b+20*c))%10)/5;
     f = ((num-(100*a+50*b+20*c+10*d))%5)/2;
     g = (num-(100*a+50*b+20*c+10*d+5*e))%2;
-    printf("%d nota(s) de R$ 100,00\n%d nota(s) de R$ 50,00\n%d nota(s) de R$ 20,00\n%d nota(s) de R$ 10,00\n%d nota(s) de R$ 5,00\n%d nota(s) de R$ 2,00\n%d nota(s) de R$ 1,00\n", a, b, c, d, e, f, g);
+    printf("%d\n%d nota(s) de R$ 100,00\n%d nota(s) de R$ 50,00\n%d nota(s) de R$ 20,00\n%d nota(s) de R$ 10,00\n%d nota(s) de R$ 5,00\n%d nota(s) de R$ 2,00\n%d nota(s) de R$ 1,00\n", num, a, b, c, d, e, f, g);
     return 0;
 }
